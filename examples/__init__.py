62a892b53ad5a65921305c64546c1333c23f6c93
Yeongseon Choe <yeongseon.choe@gmail.com>
2026-04-22 14:45:42 +0900

    chore(examples): promote queue scheduling fixture to examples module (#102)
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ examples/__init__.py
@@ -0,0 +1,3 @@
+from . import queue_scheduling
+
+__all__ = ("queue_scheduling",)
---
+++ examples/queue_scheduling/__init__.py
@@ -0,0 +1,3 @@
+from .domain import Job, QueueProposal, QueueScenario, Slot, Worker
+
+__all__ = ("Job", "QueueProposal", "QueueScenario", "Slot", "Worker")
