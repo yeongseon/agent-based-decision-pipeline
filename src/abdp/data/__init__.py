44c91d480692719d76634b74bc11b5dc2b728c97
Yeongseon Choe <yeongseon.choe@gmail.com>
2026-04-22 10:03:23 +0900

    feat(data): add GoldContract protocol (#31)

--- src/abdp/data/__init__.py
+++ src/abdp/data/__init__.py
@@ -1,7 +1,14 @@
 """"""
 
 from abdp.data.bronze import BronzeContract
+from abdp.data.gold import GoldContract
 from abdp.data.silver import SilverContract
 from abdp.data.snapshot_manifest import SnapshotManifest, SnapshotTier
 
-__all__ = ["BronzeContract", "SilverContract", "SnapshotManifest", "SnapshotTier"]
+__all__ = [
+    "BronzeContract",
+    "GoldContract",
+    "SilverContract",
+    "SnapshotManifest",
+    "SnapshotTier",
+]
