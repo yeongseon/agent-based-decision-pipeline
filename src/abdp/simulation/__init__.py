eb0f89745d71ebc09102bb3883b99d3f1ad69d7c
Yeongseon Choe <yeongseon.choe@gmail.com>
2026-04-22 10:47:34 +0900

    feat(simulation): add SegmentState protocol (#35)

--- src/abdp/simulation/__init__.py
+++ src/abdp/simulation/__init__.py
@@ -1,9 +1,11 @@
 """"""
 
 from abdp.simulation.participant_state import ParticipantState
+from abdp.simulation.segment_state import SegmentState
 from abdp.simulation.snapshot_ref import SnapshotRef
 
 globals().pop("participant_state", None)
+globals().pop("segment_state", None)
 globals().pop("snapshot_ref", None)
 
-__all__ = ["ParticipantState", "SnapshotRef"]
+__all__ = ["ParticipantState", "SegmentState", "SnapshotRef"]
